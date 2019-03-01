d8aad3ff0778f2e7219bea1e7efc94e6e13a12f0
Akshar <akshar.rawal@thomsonreuters.com>
2019-03-01 12:55:19 -0500

    with attachments

--- createitem.go
+++ createitem.go
@@ -1,13 +1,30 @@
-// 26 august 2016
 package ews
 
 import (
 	"encoding/xml"
 )
 
+type EmailMetadata struct {
+	Action  string // SaveOnly or SendAndSaveCopy
+	To      []string
+	Cc      []string
+	Bcc     []string
+	ReplyTo string
+	Subject string
+	Body    string
+	Type    string // Text or HTML
+	Folder  string
+}
+
+type AttachmentMetadata struct {
+	Name           string
+	Content        string
+	EmailID        string
+	EmailChangeKey string
+}
+
 // https://msdn.microsoft.com/en-us/library/office/aa563009(v=exchg.140).aspx
 
-// CreateItem ...
 type CreateItem struct {
 	XMLName            struct{}          `xml:"m:CreateItem"`
 	MessageDisposition string            `xml:"MessageDisposition,attr"`
@@ -15,67 +32,155 @@ type CreateItem struct {
 	Items              Messages          `xml:"m:Items"`
 }
 
-// Messages ...
 type Messages struct {
 	Message []Message `xml:"t:Message"`
 }
 
-// SavedItemFolderID ...
 type SavedItemFolderID struct {
 	DistinguishedFolderID DistinguishedFolderID `xml:"t:DistinguishedFolderId"`
 }
 
-// DistinguishedFolderID ...
 type DistinguishedFolderID struct {
 	ID string `xml:"Id,attr"`
 }
 
-// Message ...
 type Message struct {
-	ItemClass    string     `xml:"t:ItemClass"`
-	Subject      string     `xml:"t:Subject"`
-	Body         Body       `xml:"t:Body"`
-	Sender       OneMailbox `xml:"t:Sender"`
-	ToRecipients XMailbox   `xml:"t:ToRecipients"`
+	ItemClass   string       `xml:"t:ItemClass"`
+	Subject     string       `xml:"t:Subject"`
+	Body        Body         `xml:"t:Body"`
+	Attachments []Attachment `xml:"t:Attachments"`
+	// Sender       OneMailbox   `xml:"t:Sender"`
+	ToRecipients  XMailbox   `xml:"t:ToRecipients"`
+	CcRecipients  XMailbox   `xml:"t:CcRecipients"`
+	BccRecipients XMailbox   `xml:"t:BccRecipients"`
+	ReplyTo       OneMailbox `xml:"t:ReplyTo"`
 }
 
-// Body ...
 type Body struct {
 	BodyType string `xml:"BodyType,attr"`
 	Body     []byte `xml:",chardata"`
 }
 
-// OneMailbox ...
 type OneMailbox struct {
 	Mailbox Mailbox `xml:"t:Mailbox"`
 }
 
-// XMailbox ...
 type XMailbox struct {
 	Mailbox []Mailbox `xml:"t:Mailbox"`
 }
 
-// Mailbox ...
 type Mailbox struct {
 	EmailAddress string `xml:"t:EmailAddress"`
 }
 
-// BuildTextEmail ...
-func BuildTextEmail(from string, to []string, subject string, body []byte) ([]byte, error) {
+type FileAttachment struct {
+	Name           string `xml:"t:Name"`
+	IsInline       bool   `xml:"t:IsInline"`
+	IsContactPhoto bool   `xml:"t:IsContactPhoto"`
+	Content        string `xml:"t:Content"`
+}
+
+type Attachment struct {
+	FileAttachment FileAttachment `xml:"t:FileAttachment"`
+}
+
+type ItemID struct {
+	ID        string `xml:"Id,attr"`
+	ChangeKey string `xml:"ChangeKey,attr"`
+}
+
+type ItemIDs struct {
+	ItemID []ItemID `xml:"t:ItemId"`
+}
+
+type EmailAttachment struct {
+	XMLName      xml.Name     `xml:"CreateAttachment"`
+	Xmlns        string       `xml:"xmlns,attr"`
+	XmlnsT       string       `xml:"xmlns:t,attr"`
+	ParentItemID ItemID       `xml:"ParentItemId"`
+	Attachments  []Attachment `xml:"Attachments"`
+}
+
+type SendItem struct {
+	XMLName           xml.Name `xml:"SendItem"`
+	Xmlns             string   `xml:"xmlns,attr"`
+	SaveItemToFolder  bool     `xml:"SaveItemToFolder,attr"`
+	ItemIds           ItemIDs  `xml:"ItemIds"`
+	SavedItemFolderID string   `xml:"t:SavedItemFolderId"`
+}
+
+func BuildTextEmail(metadata EmailMetadata) ([]byte, error) {
+	email := CreateItemXML(metadata)
+	return xml.MarshalIndent(email, "", "  ")
+}
+
+func BuildAttachment(metadata AttachmentMetadata) ([]byte, error) {
+	var emailAttachment EmailAttachment
+	emailAttachment.Xmlns = "http://schemas.microsoft.com/exchange/services/2006/messages"
+	emailAttachment.XmlnsT = "http://schemas.microsoft.com/exchange/services/2006/types"
+	emailAttachment.ParentItemID.ID = metadata.EmailID
+	emailAttachment.ParentItemID.ChangeKey = metadata.EmailChangeKey
+
+	attachment := Attachment{FileAttachment{
+		Name:    metadata.Name,
+		Content: metadata.Content,
+	}}
+	emailAttachment.Attachments = []Attachment{attachment}
+
+	return xml.MarshalIndent(emailAttachment, "", "  ")
+}
+
+func BuildSendSavedEmail(emailId, changeKey string) ([]byte, error) {
+	var senditem SendItem
+	senditem.Xmlns = "http://schemas.microsoft.com/exchange/services/2006/messages"
+	senditem.SaveItemToFolder = true
+	senditem.SavedItemFolderID = "sentitems"
+
+	senditem.ItemIds = ItemIDs{
+		ItemID: []ItemID{
+			ItemID{
+				ID:        emailId,
+				ChangeKey: changeKey,
+			},
+		},
+	}
+
+	return xml.MarshalIndent(senditem, "", "  ")
+}
+
+func CreateItemXML(metadata EmailMetadata) CreateItem {
 	c := new(CreateItem)
-	c.MessageDisposition = "SendAndSaveCopy"
-	c.SavedItemFolderID.DistinguishedFolderID.ID = "sentitems"
+	c.MessageDisposition = metadata.Action
+	c.SavedItemFolderID.DistinguishedFolderID.ID = metadata.Folder
 	m := new(Message)
 	m.ItemClass = "IPM.Note"
-	m.Subject = subject
+	m.Subject = metadata.Subject
 	m.Body.BodyType = "Text"
-	m.Body.Body = body
-	m.Sender.Mailbox.EmailAddress = from
-	mb := make([]Mailbox, len(to))
-	for i, addr := range to {
+	m.Body.Body = []byte(metadata.Body)
+
+	// adding a sender requires "SendAs" on the account sending the message, even if it sending email from itself
+	// m.Sender.Mailbox.EmailAddress = metadata.From
+
+	mb := make([]Mailbox, len(metadata.To))
+	for i, addr := range metadata.To {
 		mb[i].EmailAddress = addr
 	}
 	m.ToRecipients.Mailbox = append(m.ToRecipients.Mailbox, mb...)
+
+	mb = make([]Mailbox, len(metadata.Cc))
+	for i, addr := range metadata.Cc {
+		mb[i].EmailAddress = addr
+	}
+	m.CcRecipients.Mailbox = append(m.CcRecipients.Mailbox, mb...)
+
+	mb = make([]Mailbox, len(metadata.Bcc))
+	for i, addr := range metadata.Bcc {
+		mb[i].EmailAddress = addr
+	}
+	m.BccRecipients.Mailbox = append(m.BccRecipients.Mailbox, mb...)
+
+	m.ReplyTo.Mailbox.EmailAddress = metadata.ReplyTo
 	c.Items.Message = append(c.Items.Message, *m)
-	return xml.MarshalIndent(c, "", "  ")
+
+	return *c
 }
--- ews.go
+++ ews.go
@@ -1,14 +1,75 @@
-// 26 august 2016
 package ews
 
 import (
 	"bytes"
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 // https://msdn.microsoft.com/en-us/library/office/dd877045(v=exchg.140).aspx
 // https://arvinddangra.wordpress.com/2011/09/29/send-email-using-exchange-smtp-and-ews-exchange-web-service/
 // https://msdn.microsoft.com/en-us/library/office/dn789003(v=exchg.150).aspx
+
+type Credentials struct {
+	Server   string
+	Username string
+	Password string
+}
+
+type CreateItemResponse struct {
+	XMLName xml.Name `xml:"Envelope"`
+	Body    struct {
+		CreateItemResponseMessage struct {
+			ResponseClass string `xml:"ResponseClass,attr"`
+			ResponseCode  string `xml:"ResponseCode"`
+			Items         struct {
+				Message []struct {
+					ItemID struct {
+						ID        string `xml:"Id,attr"`
+						ChangeKey string `xml:"ChangeKey,attr"`
+					} `xml:"ItemId"`
+				} `xml:"Message"`
+			} `xml:"Items"`
+		} `xml:"CreateItemResponse>ResponseMessages>CreateItemResponseMessage"`
+	} `xml:"Body"`
+}
+
+type CreateAttachmentResponse struct {
+	XMLName xml.Name `xml:"Envelope"`
+	Body    struct {
+		CreateAttachmentResponseMessage struct {
+			ResponseClass string `xml:"ResponseClass,attr"`
+			ResponseCode  string `xml:"ResponseCode"`
+			Attachments   struct {
+				FileAttachment []struct {
+					AttachmentID struct {
+						ID                string `xml:"Id,attr"`
+						RootItemID        string `xml:"RootItemId,attr"`
+						RootItemChangeKey string `xml:"RootItemChangeKey,attr"`
+					} `xml:"AttachmentId"`
+					LastModifiedTime string `xml:"LastModifiedTime"`
+				} `xml:"FileAttachment"`
+			} `xml:"Attachments"`
+		} `xml:"CreateAttachmentResponse>ResponseMessages>CreateAttachmentResponseMessage"`
+	} `xml:"Body"`
+}
+
+type SendItemResponse struct {
+	XMLName xml.Name `xml:"Envelope"`
+	Body    struct {
+		SendItemResponse struct {
+			ResponseMessages struct {
+				SendItemResponseMessage struct {
+					ResponseClass string `xml:"ResponseClass,attr"`
+					ResponseCode  string `xml:"ResponseCode"`
+				} `xml:"SendItemResponseMessage"`
+			} `xml:"ResponseMessages"`
+		} `xml:"SendItemResponse"`
+	} `xml:"Body"`
+}
 
 var soapheader = `<?xml version="1.0" encoding="utf-8" ?>
 <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
@@ -18,17 +79,135 @@ var soapheader = `<?xml version="1.0" encoding="utf-8" ?>
   <soap:Body>
 `
 
-func Issue(ewsAddr string, username string, password string, body []byte) (*http.Response, error) {
+func SendEmailWithAttachment(creds Credentials, emailMetadata EmailMetadata, attachmentMetadata AttachmentMetadata) (string, error) {
+	emailMetadata.Action = "SaveOnly"
+	emailMetadata.Folder = "sentitems"
+
+	var err error
+	_, attachmentMetadata.EmailID, attachmentMetadata.EmailChangeKey, err = IssueTextEmail(creds, emailMetadata)
+	if err != nil {
+		return "", err
+	}
+
+	attachmentMetadata.EmailChangeKey, err = IssueAttachment(creds, attachmentMetadata)
+	if err != nil {
+		return "", err
+	}
+
+	return IssueEmailWithAttachment(creds, attachmentMetadata)
+}
+
+func SendEmail(creds Credentials, metadata EmailMetadata) (string, error) {
+	metadata.Action = "SendAndSaveCopy"
+	if metadata.Folder == "" {
+		metadata.Folder = "sentitems"
+	}
+	status, _, _, err := IssueTextEmail(creds, metadata)
+	return status, err
+}
+
+func IssueEmailWithAttachment(creds Credentials, metadata AttachmentMetadata) (string, error) {
+	sendXML, err := BuildSendSavedEmail(metadata.EmailID, metadata.EmailChangeKey)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := Issue(creds, sendXML)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var sendResp SendItemResponse
+	err = xml.Unmarshal([]byte(resp), &sendResp)
+	if err != nil {
+		return "", err
+	}
+
+	if sendResp.Body.SendItemResponse.ResponseMessages.SendItemResponseMessage.ResponseClass != "Success" {
+		return "", errors.New(sendResp.Body.SendItemResponse.ResponseMessages.SendItemResponseMessage.ResponseCode)
+	}
+
+	return sendResp.Body.SendItemResponse.ResponseMessages.SendItemResponseMessage.ResponseClass, nil
+}
+
+func IssueAttachment(creds Credentials, metadata AttachmentMetadata) (string, error) {
+	attachmentXML, err := BuildAttachment(metadata)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := Issue(creds, attachmentXML)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var createAttachmentResponse CreateAttachmentResponse
+	err = xml.Unmarshal([]byte(resp), &createAttachmentResponse)
+	if err != nil {
+		return "", err
+	}
+
+	return createAttachmentResponse.Body.CreateAttachmentResponseMessage.Attachments.FileAttachment[0].AttachmentID.RootItemChangeKey, nil
+}
+
+func IssueTextEmail(creds Credentials, metadata EmailMetadata) (string, string, string, error) {
+	emailXML, err := BuildTextEmail(metadata)
+
+	resp, err := Issue(creds, emailXML)
+	if err != nil {
+		return "", "", "", err
+	}
+
+	var createResponse CreateItemResponse
+	err = xml.Unmarshal([]byte(resp), &createResponse)
+	if err != nil {
+		return "", "", "", err
+	}
+
+	if createResponse.Body.CreateItemResponseMessage.ResponseClass != "Success" {
+		return "", "", "", errors.New(createResponse.Body.CreateItemResponseMessage.ResponseCode)
+	}
+
+	if metadata.Action == "SendAndSaveCopy" {
+		return createResponse.Body.CreateItemResponseMessage.ResponseClass, "", "", nil
+	}
+
+	return createResponse.Body.CreateItemResponseMessage.ResponseClass, createResponse.Body.CreateItemResponseMessage.Items.Message[0].ItemID.ID, createResponse.Body.CreateItemResponseMessage.Items.Message[0].ItemID.ChangeKey, nil
+}
+
+func Issue(creds Credentials, body []byte) (string, error) {
 	b2 := []byte(soapheader)
 	b2 = append(b2, body...)
 	b2 = append(b2, "\n  </soap:Body>\n</soap:Envelope>"...)
-	req, err := http.NewRequest("POST", ewsAddr, bytes.NewReader(b2))
+	req, err := http.NewRequest("POST", creds.Server, bytes.NewReader(b2))
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	req.SetBasicAuth(username, password)
+	req.SetBasicAuth(creds.Username, creds.Password)
 	req.Header.Set("Content-Type", "text/xml")
 	client := new(http.Client)
 	client.CheckRedirect = func(req *http.Request, via []*http.Request) error { return http.ErrUseLastResponse }
-	return client.Do(req)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+		bodyString := string(bodyBytes)
+		return bodyString, nil
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	bodyString := string(bodyBytes)
+	fmt.Println(bodyString)
+	return bodyString, errors.New(resp.Status)
 }
