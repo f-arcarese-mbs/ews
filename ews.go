c274290b4f9f76782451f6ec15eda8e8541693b7
sundog <francesco.arcarese@mangrovia.solutions>
2022-09-27 16:09:02 +0200

    added OAuth2 authentication

--- ews.go
+++ ews.go
@@ -2,11 +2,17 @@ package ews
 
 import (
 	"bytes"
+	"encoding/json"
 	"encoding/xml"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // https://msdn.microsoft.com/en-us/library/office/dd877045(v=exchg.140).aspx
@@ -14,9 +20,22 @@ import (
 // https://msdn.microsoft.com/en-us/library/office/dn789003(v=exchg.150).aspx
 
 type Credentials struct {
-	Server   string
-	Username string
-	Password string
+	Server       string
+	Username     string
+	Password     string
+	Tenant       string
+	ClientID     string
+	ClientSecret string
+	GrantType    string
+	Scope        string
+	URL          string
+}
+
+type OAuthResponse struct {
+	TokenType    string `json:"token_type"`
+	ExpiresIn    int    `json:"expires_is"`
+	ExtExpiresIn int    `json:"ext_expires_in"`
+	AccessToken  string `json:"access_token"`
 }
 
 type CreateItemResponse struct {
@@ -71,16 +90,7 @@ type SendItemResponse struct {
 	} `xml:"Body"`
 }
 
-var soapheader = `<?xml version="1.0" encoding="utf-8" ?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Header>
-    <t:RequestServerVersion Version="Exchange2007_SP1" />
-  </soap:Header>
-  <soap:Body>
-`
-
 func SendEmailWithAttachment(creds Credentials, emailMetadata EmailMetadata, attachmentMetadata AttachmentMetadata) (string, error) {
-	fmt.Println("AAAAAAAAAAAAAAAAOOOOOOOOOOOOO&&&&&&&&&&&")
 	emailMetadata.Action = "SaveOnly"
 	emailMetadata.Folder = "sentitems"
 
@@ -99,7 +109,6 @@ func SendEmailWithAttachment(creds Credentials, emailMetadata EmailMetadata, att
 }
 
 func SendEmail(creds Credentials, metadata EmailMetadata) (string, error) {
-	fmt.Println("AAAAAAAAAAAAAAAAOOOOOOOOOOOOO")
 	metadata.Action = "SendAndSaveCopy"
 	if metadata.Folder == "" {
 		metadata.Folder = "sentitems"
@@ -178,6 +187,29 @@ func IssueTextEmail(creds Credentials, metadata EmailMetadata) (string, string, 
 }
 
 func Issue(creds Credentials, body []byte) (string, error) {
+	soapheader := `<?xml version="1.0" encoding="utf-8" ?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Header>
+    <t:RequestServerVersion Version="Exchange2007_SP1" />
+   </soap:Header>
+   <soap:Body>
+`
+
+	if creds.ClientID != "" {
+		soapheader = `<?xml version="1.0" encoding="utf-8" ?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Header>
+	<t:RequestServerVersion Version="Exchange2007_SP1" />
+	<t:ExchangeImpersonation>
+	<t:ConnectingSID>
+	  <t:PrimarySmtpAddress>` + creds.Username + `</t:PrimarySmtpAddress>
+	</t:ConnectingSID>
+  </t:ExchangeImpersonation>
+  </soap:Header>
+  <soap:Body>
+`
+	}
+
 	b2 := []byte(soapheader)
 	b2 = append(b2, body...)
 	b2 = append(b2, "\n  </soap:Body>\n</soap:Envelope>"...)
@@ -185,7 +217,28 @@ func Issue(creds Credentials, body []byte) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	req.SetBasicAuth(creds.Username, creds.Password)
+
+	if creds.ClientID != "" {
+		oauthResponse, err := requestOAuthToken(creds)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return "", err
+		}
+
+		if oauthResponse.AccessToken == "" || oauthResponse.TokenType == "" {
+			err := errors.New("No access token")
+			fmt.Println(err.Error())
+			return "", err
+		}
+
+		req.Header.Set("Authorization", oauthResponse.TokenType+" "+oauthResponse.AccessToken)
+		req.Header.Set("X-PreferServerAffinity", "true")
+		req.Header.Set("X-AnchorMailbox", creds.Username)
+	} else {
+		req.SetBasicAuth(creds.Username, creds.Password)
+	}
+
 	req.Header.Set("Content-Type", "text/xml")
 	client := new(http.Client)
 	client.CheckRedirect = func(req *http.Request, via []*http.Request) error { return http.ErrUseLastResponse }
@@ -212,4 +265,51 @@ func Issue(creds Credentials, body []byte) (string, error) {
 	bodyString := string(bodyBytes)
 	fmt.Println(bodyString)
 	return bodyString, errors.New(resp.Status)
+}
+
+func requestOAuthToken(creds Credentials) (OAuthResponse, error) {
+	ret := OAuthResponse{}
+
+	client := &http.Client{
+		Timeout: time.Second * 30,
+	}
+
+	data := url.Values{}
+	data.Set("grant_type", creds.GrantType)
+	data.Set("client_id", creds.ClientID)
+	data.Set("client_secret", creds.ClientSecret)
+	data.Set("scope", creds.Scope)
+
+	encodedData := data.Encode()
+	req, err := http.NewRequest("GET", fmt.Sprintf(creds.URL, creds.Tenant), strings.NewReader(encodedData))
+
+	if err != nil {
+		fmt.Printf("Error %s", err.Error())
+		return ret, err
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error %s", err.Error())
+		return ret, err
+	}
+
+	defer response.Body.Close()
+
+	b, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Error s%s", err.Error())
+		return ret, err
+	}
+
+	err = json.Unmarshal(b, &ret)
+	if err != nil {
+		fmt.Printf("Error %s", err.Error())
+		return ret, err
+	}
+
+	return ret, nil
 }
